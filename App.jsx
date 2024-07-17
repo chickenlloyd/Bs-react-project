6358dc25d6c44ca9af530c7277f52985cc90c205
chickenlloyd <alizadeh.mason@gmail.com>
2024-07-16 19:08:17 -0700

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,19 +1,39 @@
+
 import Cards from "./Cards";
-import DrawFiveCards from "./drawFiveCards";
+import GameArea from "./GameArea";
+
+import { createContext, useState } from "react";
+
+export const GameContext = createContext(null);
 
 function App() {
-
-  const x = chance.string()
-  
-
+  const [gameInfo, setGameInfo] = useState({
+    cardToPlay: 1,
+    currentCard: null,
+    player1Turn:true,
+    player2Turn:false,
+    hasPlayedCard:false,
+    errorMessage:""
+  });
 
   return (
-    <div>
-      <h1 className="text-3xl text-center m-5">Mason's BS Project</h1>
-      <Cards />
-      <DrawFiveCards />
-
-    </div>
+    <GameContext.Provider
+    value={{
+      gameInfo,
+      setGameInfo
+    }}>
+      <div>
+        <h1 className="text-3xl text-center m-5">Mason's BS Project</h1>
+        
+        <div className="flex flex-col items-center">
+            <Cards ownCards={true}/>
+            
+            <GameArea />
+            
+            <Cards ownCards={false}/>
+          </div>
+      </div>
+      </GameContext.Provider>
   )
 }
 
